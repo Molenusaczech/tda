e65a218464a3ef2ac68dcc642b9844ed7e704e02
Daniel Rajtšlégr <80426398+Molenusaczech@users.noreply.github.com>
2022-11-12 11:09:32 +0100

    Add files via upload

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -9,4 +9,42 @@ use Nette;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+
+    public function renderDefault(): void
+    {
+        $data = file_get_contents('../storage/placeholderdata.json');
+        $data = json_decode($data, true);
+
+        $notes = $data['notes'];
+        $tags = $data['tags'];
+
+        $finalNotes = array();
+
+        $index = 0;
+        foreach ($notes as $note) {
+            $finalNotes[$index] = $note;
+            $finalNotes[$index]['data'] = 'data-note-date='.$note["date"];
+
+            $index2 = 0;
+            foreach ($note['tags'] as $tag) {
+                
+                $color = $tags[$tag]['color'];
+
+                $finalNotes[$index]['tags'][$index2] = array(
+                    'name' => $tag,
+                    'style' => 'background-color: '.$color
+                );
+                
+
+                $index2++;
+            }
+
+            $index++;
+        }
+
+        $this->template->notes = $finalNotes;
+
+    }
+
+
 }
