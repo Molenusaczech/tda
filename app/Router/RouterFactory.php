fbdae3c6fcab2f42b87ca0f477c3115b8a74f965
Daniel Rajtšlégr <06krtek@gmail.com>
2023-03-31 14:05:45 +0200

    factory reset the repo, oh shit here we go again

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -16,12 +16,6 @@ final class RouterFactory
 	{
 		$router = new RouteList;
 		$router->addRoute('<presenter>/<action>[/<id>]', 'Homepage:default');
-		$router->addRoute('login[/<id>]', 'Login:default');
-		$router->addRoute('accountmanager[/<id>]', 'AccountManager:default');
-		$router->addRoute('backupmanager[/<id>]', 'BackupManager:default');
-		$router->addRoute('backupdownload/<id>', 'BackupManager:download');
-		$router->addRoute('users/<user_id>/records/<record_id>', 'Api:default');
-		$router->addRoute('users/<user_id>/records/', 'Api:secondary');
 		return $router;
 	}
 }
